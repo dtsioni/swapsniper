0187d05eea592715fd83bb47a9b9bc5026bd58b5
Daniel Tsioni <dtsioni@gmail.com>
2015-08-18 21:10:36 -0400

    Ran migrations for user origin and destination foreign keys

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819010349) do
+ActiveRecord::Schema.define(version: 20150819010947) do
 
   create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150819010349) do
     t.string   "last_name"
     t.string   "campus"
     t.string   "building"
+    t.integer  "origin_id"
+    t.integer  "destination_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
