e134b32a73a65556347ce740c59881ca8492a774
Daniel Tsioni <dtsioni@gmail.com>
2015-08-19 19:00:57 -0400

    Ran migration to add gender to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819010947) do
+ActiveRecord::Schema.define(version: 20150819230027) do
 
   create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20150819010947) do
     t.string   "building"
     t.integer  "origin_id"
     t.integer  "destination_id"
+    t.string   "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
