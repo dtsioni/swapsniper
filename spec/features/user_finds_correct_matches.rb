0e114be81674bd6c6937b6250e5a24d658ff456a
Daniel Tsioni <dtsioni@gmail.com>
2015-08-23 16:17:47 -0400

    Made partial test

--- spec/features/user_finds_correct_matches.rb
+++ spec/features/user_finds_correct_matches.rb
@@ -1,4 +1,12 @@
 require "spec_helper"
 
 describe "User goes to matches page" do
+  before do
+    @user = FactoryGirl.build(:user, :male)
+
+    visit sign_in_path
+
+    fill_in :first_name, with: @user.first_name
+    fill_in :last_name, with: @user.last_name
+  end
 end
