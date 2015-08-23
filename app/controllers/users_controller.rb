67032abe85cbe94f9d50d8f39c305d2e8b3cca2a
Daniel Tsioni <dtsioni@gmail.com>
2015-08-23 16:12:14 -0400

    Made user accept nested attributes in form and creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,17 @@ class UsersController < Clearance::UsersController
   def matches
   end
 
+  def new
+    @user = User.new
+    @origin = Origin.new
+    @destination = Destination.new
+
+    @user.origin = @origin
+    @user.destination = @destination
+
+    render template: "users/new"
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :first_name, :last_name, :gender)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   include Clearance::User
   belongs_to :origin
   belongs_to :destination
+  accepts_nested_attributes_for :origin
+  accepts_nested_attributes_for :destination
 
   validates_presence_of :first_name, :last_name, :gender
 
