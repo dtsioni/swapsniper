92a52e87aa23dc44d4b06afbb3e0ec7f95ba7855
Daniel Tsioni <dtsioni@gmail.com>
2015-08-23 17:00:45 -0400

    Ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819230027) do
+ActiveRecord::Schema.define(version: 20150823205423) do
 
   create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "campus"
     t.string   "building"
+    t.string   "type"
   end
 
   create_table "users", force: :cascade do |t|
