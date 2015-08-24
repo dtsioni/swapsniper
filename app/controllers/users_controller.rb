4aeeefdf0c6fcb3190dbb63d0fde9d6dc4ea4930
Daniel Tsioni <dtsioni@gmail.com>
2015-08-24 13:09:52 -0400

    Overrode user create method to user find or create by methods for user origin and destination, and made user params method private

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,22 @@ class UsersController < Clearance::UsersController
     render template: "users/new"
   end
 
+  def create
+    @user = user_from_params
+
+    @user.origin = Origin.find_or_create_by(campus: @user.origin.campus, building: @user.origin.building)
+    @user.destination = Destination.find_or_create_by(campus: @user.destination.campus, building: @user.destination.building)
+
+    if @user.save
+      sign_in @user
+      redirect_back_or url_after_create
+    else
+      render template: "users/new"
+    end
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:email, :password, :first_name, :last_name, :gender,
       destination_attributes: [:campus, :building], origin_attributes: [:campus, :building])
