648f01a7deb6599cff017508fe951a306701ab93
Daniel Tsioni <dtsioni@gmail.com>
2015-08-25 10:59:12 -0400

    Add role to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150823205423) do
+ActiveRecord::Schema.define(version: 20150825143138) do
 
   create_table "locations", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150823205423) do
     t.integer  "origin_id"
     t.integer  "destination_id"
     t.string   "gender"
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email"
