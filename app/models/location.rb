619ac581287858ed81458e07f749cbbf4685e50d
Daniel Tsioni <dtsioni@gmail.com>
2015-08-26 12:25:49 -0400

    Added building and campus info to location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,8 @@
 class Location < ActiveRecord::Base
+  CAMPUSES = ["livingston", "college avenue", "busch", "cook/douglass"]
+
+  BUILDINGS_LIVINGSTON = ["livingston apartments", "lynton towers north", "lynton towers south", "quad 1", "quad 2", "quad 3"]
+  BUILDINGS_BUSCH = ["allen hall", "barr hall", "best", "crosby suites", "davidson hall", "judson suites", "mattia hall", "mccormick suites", "metzger hall", "morrow suites", "nichols apartments", "richardson apartments", "silvers apartments", "thomas suites", "winkler suites", "marvin apartments", "buell apartments", "johnson apartments"]
+  BUILDINGS_COLLEGE_AVENUE = ["brett hall", "campbell hall", "clothier hall", "demarest hall", "frelinghuysen hall", "hardenbergh hall", "hegeman hall", "leupp hall", "mettler hall", "pell hall", "stonier hall", "tinsley hall", "university center at easton ave apartments", "wessels hall"]
+  BUILDINGS_COOK_DOUGLASS = ["heylar house", "henderson apartments", "jameson hall", "katzenbach hall", "lippincott hall", "new gibbons hall", "newell apartments", "nicholas hall", "old gibbons hall", "perry hall", "voorhees hall", "woodbury bunting-cobb hall", "starkey apartments"]
 end
