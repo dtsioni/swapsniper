4961883625a4183bbce8642d23a2897e7b6af550
Daniel Tsioni <dtsioni@gmail.com>
2015-08-29 15:41:57 -0400

    Moved inline js out of new user and change location user forms into seperate js file

---
+++ app/assets/javascripts/location.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+  selected_building = $("#user_origin_attributes_campus>option:selected").text().toLowerCase();
+  var buildings = "";
+  switch(selected_building){
+    case "busch":
+      buildings = BUILDINGS_BUSCH;
+      break;
+    case "college avenue":
+      buildings = BUILDINGS_COLLEGE_AVENUE;
+      break;
+    case "cook/douglass":
+      buildings = BUILDINGS_COOK_DOUGLASS;
+      break;
+    case "livingston":
+      buildings = BUILDINGS_LIVINGSTON;
+  }
+  var newOptions = "";
+  for(i = 0; i < buildings.length; i++){
+    newOptions += "<option value='" + buildings[i] + "'>" + buildings[i].toTitleCase() + "</option>";
+  }
+  $('#user_origin_attributes_building').html(newOptions);
+  $('#user_origin_attributes_campus').change(function(){
+    selected_building = $("#user_origin_attributes_campus>option:selected").text().toLowerCase();
+    var buildings = "";
+    switch(selected_building){
+      case "busch":
+        buildings = BUILDINGS_BUSCH;
+        break;
+      case "college avenue":
+        buildings = BUILDINGS_COLLEGE_AVENUE;
+        break;
+      case "cook/douglass":
+        buildings = BUILDINGS_COOK_DOUGLASS;
+        break;
+      case "livingston":
+        buildings = BUILDINGS_LIVINGSTON;
+    }
+    var newOptions = "";
+    for(i = 0; i < buildings.length; i++){
+      newOptions += "<option value='" + buildings[i] + "'>" + buildings[i].toTitleCase() + "</option>";
+    }
+    $('#user_origin_attributes_building').html(newOptions);
+  });
+  //destination
+  selected_building = $("#user_destination_attributes_campus>option:selected").text().toLowerCase();
+  var buildings = "";
+  switch(selected_building){
+    case "busch":
+      buildings = BUILDINGS_BUSCH;
+      break;
+    case "college avenue":
+      buildings = BUILDINGS_COLLEGE_AVENUE;
+      break;
+    case "cook/douglass":
+      buildings = BUILDINGS_COOK_DOUGLASS;
+      break;
+    case "livingston":
+      buildings = BUILDINGS_LIVINGSTON;
+  }
+  var newOptions = "";
+  for(i = 0; i < buildings.length; i++){
+    newOptions += "<option value='" + buildings[i] + "'>" + buildings[i].toTitleCase() + "</option>";
+  }
+  $('#user_destination_attributes_building').html(newOptions);
+  $('#user_destination_attributes_campus').change(function(){
+    selected_building = $("#user_destination_attributes_campus>option:selected").text().toLowerCase();
+    var buildings = "";
+    switch(selected_building){
+      case "busch":
+        buildings = BUILDINGS_BUSCH;
+        break;
+      case "college avenue":
+        buildings = BUILDINGS_COLLEGE_AVENUE;
+        break;
+      case "cook/douglass":
+        buildings = BUILDINGS_COOK_DOUGLASS;
+        break;
+      case "livingston":
+        buildings = BUILDINGS_LIVINGSTON;
+    }
+    var newOptions = "";
+    for(i = 0; i < buildings.length; i++){
+      newOptions += "<option value='" + buildings[i] + "'>" + buildings[i].toTitleCase() + "</option>";
+    }
+    $('#user_destination_attributes_building').html(newOptions);
+  });
+});
