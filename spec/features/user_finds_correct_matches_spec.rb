b5ebf2d816bd85d00e71970ab829386115cd73d2
Daniel Tsioni <dtsioni@gmail.com>
2015-08-29 18:52:23 -0400

    Renamed test to end in spec

--- spec/features/user_finds_correct_matches_spec.rb
+++ spec/features/user_finds_correct_matches_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 
 describe "User goes to matches page" do
+  subject{ page }
   before do
     @user = FactoryGirl.create(:user, :male)
 
@@ -8,5 +9,12 @@ describe "User goes to matches page" do
 
     fill_in :email, with: @user.email
     fill_in :password, with: @user.password
+
+    @user.origin = FactoryGirl.create(:origin)
+    @user.destination = FactoryGirl.create(:destination)
+
+    click_button "Sign in"
   end
+
+  it{ should have_content("no matches") }
 end
