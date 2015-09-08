2ef3b74fd1de461aee0e098f16bb843dba749682
Daniel Tsioni <dtsioni@gmail.com>
2015-09-08 18:15:01 -0400

    Fixed tests by moving require in spec helper to correct place and requiring rails helper instead of spec helper in the test

--- spec/features/user_finds_correct_matches_spec.rb
+++ spec/features/user_finds_correct_matches_spec.rb
@@ -1,4 +1,10 @@
-require "spec_helper"
+require "rails_helper"
 
 describe "User goes to matches page" do
+  subject{ page }
+  before do
+    @user = FactoryGirl.create(:user)
+    visit sign_in_path
+  end
+  it{ should have_content(@user.full_name) }
 end
