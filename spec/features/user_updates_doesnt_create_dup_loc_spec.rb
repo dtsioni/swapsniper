f893ed10776e34fd0a9b9d9b21e28738cdd40a32
Daniel Tsioni <dtsioni@gmail.com>
2015-09-09 11:27:20 -0400

    Create test for duplicate locations

---
+++ spec/features/user_updates_doesnt_create_dup_loc_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+feature "User updates their information" do
+  before do
+    @user = FactoryGirl.create(:user)
+    @user.origin = FactoryGirl.create(:origin)
+    @user.destination = FactoryGirl.create(:destination)
+    @user.save
+    visit sign_in_path
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: @user.password
+    click_button "Sign in"
+    visit edit_user_path(@user)
+  end
+
+  scenario "With the same Origin" do
+    #origin by default is livingston apartments, livingston
+    select "Livingston", from: "user_origin_attributes_campus"
+    select "Livingston Apartments", from: "user_origin_attributes_building"
+    select "Busch", from: "user_destination_attributes_campus"
+    expect{click_button "Update User"}.to_not change(Origin, :count)
+  end
+end
+
